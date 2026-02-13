1d3000bac9f5516dc10950280352eaad82c53253
Alex Jasson <alexjasson94@gmail.com>
2026-02-13 09:10:51 +0800

    improved model struct

--- src/Model.c
+++ src/Model.c
@@ -33,32 +33,25 @@ typedef struct {
     OutputLayer o;
 } Parameters;
 
-typedef Parameters Gradients; // Same data structure for our purposes
-
 typedef struct {
-    Entry *entries; // Block of memory for all matrix entries
+    Entry *entries; // Block of memory for all matrix entries in variables
+    Parameters dp;  // Gradients for each parameter (dLoss/dtheta)
     Matrix *x;      // Input vector [H x 1] for each layer
     Matrix *f;      // Forget vector [H x 1] for each layer
     Matrix *h_hat;  // Candidate activation vector [H x 1] for each layer
     Matrix *z_h;    // Pre-activation vector for h_hat [H x 1] for each layer
     Matrix *h_prev; // Previous hidden state [H x 1] for each layer
-    Matrix t1;      // Holds temporary vector values [H x 1]
-    Matrix t2;      // Holds temporary matrix values [max(V,H) x H]
-    Matrix up;      // Holds upstream gradients during backward pass [H x 1]
+    Matrix *h;      // Hidden state vectors [H x 1] for each layer
+    Matrix tempV;   // Holds 3 temporary vector values [H x 3]
+    Matrix tempM;   // Holds temporary matrix values [max(V,H) x H]
+    Matrix dh;      // Holds upstream gradient dL/dh [H x 1] during backward pass
 } Variables;
-
-typedef struct {
-    Entry *entries; // Block of memory for all matrix entries
-    Matrix *h;      // Hidden state vectors, [H x 1] for each layer
-} HiddenState;
 
 struct model
 {
     size_t V, E;    // Vocabulary size (V) and embedding size (E)
     size_t H, N;    // Size of the hidden GRU layers (H) and the number of layers (N)
     Parameters p;   // Parameters (theta)
-    Gradients g;    // Gradients (dLoss/dtheta)
-    HiddenState hs; // Hidden state
     Variables v;    // Allocated memory for calculations
 };
 
@@ -114,17 +107,17 @@ Model ModelNew(int hiddenSize, int numLayers)
     MatrixXavier(m->p.o.W);
 
     // Allocate gradients
-    m->g.h = calloc(N, sizeof(HiddenLayer));
-    m->g.entries = calloc(ModelParameters(m), sizeof(Entry));
-    if ((!m->g.entries) || (!m->g.h)) goto error;
+    m->v.dp.h = calloc(N, sizeof(HiddenLayer));
+    m->v.dp.entries = calloc(ModelParameters(m), sizeof(Entry));
+    if ((!m->v.dp.entries) || (!m->v.dp.h)) goto error;
 
     // Assign memory addresses to gradients
-    addr = m->g.entries;
+    addr = m->v.dp.entries;
 
-    m->g.e.W = MatrixView(V, E, addr); addr += V * E;
+    m->v.dp.e.W = MatrixView(V, E, addr); addr += V * E;
     for (size_t i = 0; i < N; i++)
     {
-        HiddenLayer *hl = &m->g.h[i];
+        HiddenLayer *hl = &m->v.dp.h[i];
         hl->W_f = MatrixView(H, H, addr); addr += H * H;
         hl->W_h = MatrixView(H, H, addr); addr += H * H;
         hl->U_f = MatrixView(H, H, addr); addr += H * H;
@@ -132,15 +125,8 @@ Model ModelNew(int hiddenSize, int numLayers)
         hl->b_f = MatrixView(H, 1, addr); addr += H;
         hl->b_h = MatrixView(H, 1, addr); addr += H;
     }
-    m->g.o.W = MatrixView(V, H, addr); addr += V * H;
-    m->g.o.b = MatrixView(V, 1, addr);
-
-    // Allocate hidden states and assign memory addresses
-    m->hs.h = calloc(N, sizeof(Matrix));
-    m->hs.entries = calloc(N * H, sizeof(Entry));
-    if ((!m->hs.entries) || (!m->hs.h)) goto error;
-    for (size_t i = 0; i < N; i++)
-        m->hs.h[i] = MatrixView(H, 1, m->hs.entries + i * H);
+    m->v.dp.o.W = MatrixView(V, H, addr); addr += V * H;
+    m->v.dp.o.b = MatrixView(V, 1, addr);
 
     // Allocate variables and assign memory addresses
     m->v.x      = calloc(N, sizeof(Matrix));
@@ -148,10 +134,11 @@ Model ModelNew(int hiddenSize, int numLayers)
     m->v.h_hat  = calloc(N, sizeof(Matrix));
     m->v.z_h    = calloc(N, sizeof(Matrix));
     m->v.h_prev = calloc(N, sizeof(Matrix));
+    m->v.h      = calloc(N, sizeof(Matrix));
     size_t S = V > H ? V : H;
-    m->v.entries = calloc(5 * N * H + S * H + 2 * H, sizeof(Entry));
+    m->v.entries = calloc(6 * N * H + S * H + 4 * H, sizeof(Entry));
     if (!m->v.entries || !m->v.f || !m->v.h_hat || !m->v.h_prev
-        || !m->v.x || !m->v.z_h) goto error;
+        || !m->v.x || !m->v.z_h || !m->v.h) goto error;
     addr = m->v.entries;
     for (size_t i = 0; i < N; i++) {
         m->v.x[i]      = MatrixView(H, 1, addr); addr += H;
@@ -159,10 +146,11 @@ Model ModelNew(int hiddenSize, int numLayers)
         m->v.h_hat[i]  = MatrixView(H, 1, addr); addr += H;
         m->v.z_h[i]    = MatrixView(H, 1, addr); addr += H;
         m->v.h_prev[i] = MatrixView(H, 1, addr); addr += H;
+        m->v.h[i]      = MatrixView(H, 1, addr); addr += H;
     }
-    m->v.t1 = MatrixView(H, 1, addr); addr += H;
-    m->v.t2 = MatrixView(S, H, addr); addr += S * H;
-    m->v.up = MatrixView(H, 1, addr);
+    m->v.tempV = MatrixView(H, 3, addr); addr += 3 * H;
+    m->v.tempM = MatrixView(S, H, addr); addr += S * H;
+    m->v.dh = MatrixView(H, 1, addr);
 
     return m;
 
@@ -175,18 +163,17 @@ error:
 void ModelFree(Model m)
 {
     if (!m) return;
-    free(m->hs.entries);
-    free(m->hs.h);
     free(m->v.entries);
     free(m->v.x);
     free(m->v.f);
     free(m->v.h_hat);
     free(m->v.z_h);
     free(m->v.h_prev);
+    free(m->v.h);
     free(m->p.h);
     free(m->p.entries);
-    free(m->g.h);
-    free(m->g.entries);
+    free(m->v.dp.h);
+    free(m->v.dp.entries);
     free(m);
 }
 
@@ -239,13 +226,14 @@ void ModelWrite(Model m, char *path)
 void ModelReset(Model m)
 {
     for (size_t l = 0; l < m->N; l++)
-        MatrixZero(m->hs.h[l]);
+        MatrixZero(m->v.h[l]);
 }
 
 // Accumulates the folling variables for backward pass: {x, f, h_hat, z_h, h_prev}
 void ModelForward(Model m, Token input, Matrix output)
 {
     Variables *v = &m->v;
+    Matrix tempH = MatrixView(m->H, 1, v->tempV.entries);
 
     // Embedding layer: x = W_e[input, :]^T
     Matrix x = MatrixTranspose(MatrixRow(m->p.e.W, (size_t)input));
@@ -253,34 +241,34 @@ void ModelForward(Model m, Token input, Matrix output)
     // Iterate over MGU layers
     for (size_t l = 0; l < m->N; l++)
     {
-        HiddenLayer p = m->p.h[l];  // Hidden layer parameters
-        Matrix h_prev = m->hs.h[l]; // Previous hidden state
+        HiddenLayer p = m->p.h[l]; // Hidden layer parameters
+        Matrix h_prev = m->v.h[l]; // Previous hidden state
 
         MatrixCopy(v->x[l], x);
         MatrixCopy(v->h_prev[l], h_prev);
 
         // f = sigmoid(W_f * x + U_f * h_prev + b_f)
-        MatrixMultiply(v->t1, p.U_f, h_prev);
+        MatrixMultiply(tempH, p.U_f, h_prev);
         MatrixMultiply(v->f[l], p.W_f, x);
-        MatrixAdd(v->f[l], v->f[l], v->t1);
+        MatrixAdd(v->f[l], v->f[l], tempH);
         MatrixAdd(v->f[l], v->f[l], p.b_f);
         MatrixApply(v->f[l], sigmoid);
 
         // h_hat = tanh(W_h * x + U_h * (f ⊙ h_prev) + b_h)
-        MatrixHadamard(v->t1, v->f[l], h_prev);
-        MatrixMultiply(v->t1, p.U_h, v->t1);
+        MatrixHadamard(tempH, v->f[l], h_prev);
+        MatrixMultiply(tempH, p.U_h, tempH);
         MatrixMultiply(v->z_h[l], p.W_h, x);
-        MatrixAdd(v->z_h[l], v->z_h[l], v->t1);
+        MatrixAdd(v->z_h[l], v->z_h[l], tempH);
         MatrixAdd(v->z_h[l], v->z_h[l], p.b_h);
         MatrixCopy(v->h_hat[l], v->z_h[l]);
         MatrixApply(v->h_hat[l], rationalTanh);
 
         // h_new = (1 - f) ⊙ h_prev + f ⊙ h_hat
         //       = h_prev - (f ⊙ h_prev) + (f ⊙ h_hat)
-        MatrixHadamard(v->t1, v->f[l], h_prev);
-        MatrixSubtract(h_prev, h_prev, v->t1);
-        MatrixHadamard(v->t1, v->f[l], v->h_hat[l]);
-        MatrixAdd(h_prev, h_prev, v->t1);
+        MatrixHadamard(tempH, v->f[l], h_prev);
+        MatrixSubtract(h_prev, h_prev, tempH);
+        MatrixHadamard(tempH, v->f[l], v->h_hat[l]);
+        MatrixAdd(h_prev, h_prev, tempH);
 
         // Next layer input is the previous layer output
         x = h_prev;
@@ -296,10 +284,12 @@ float ModelBackward(Model m, Token input, Token target, Matrix output)
 {
     Variables *v = &m->v;
     size_t V = m->V, H = m->H, N = m->N;
-    Matrix up = v->up;                                // Upstream gradients [H x 1]
-    Matrix tempH1 = v->t1;                            // Temporary vector [H x 1]
-    Matrix tempVxH = MatrixView(V, H, v->t2.entries); // Temporary matrix [V x H]
-    Matrix tempHxH = MatrixView(H, H, v->t2.entries); // Temporary matrix [H x H]
+    Matrix dh = v->dh;                                           // Upstream gradients [H x 1]
+    Matrix tempH1  = MatrixView(H, 1, v->tempV.entries);         // Temporary vector [H x 1]
+    Matrix tempH2  = MatrixView(H, 1, v->tempV.entries + H);     // Temporary vector [H x 1]
+    Matrix tempH3  = MatrixView(H, 1, v->tempV.entries + 2 * H); // Temporary vector [H x 1]
+    Matrix tempVxH = MatrixView(V, H, v->tempM.entries);         // Temporary matrix [V x H]
+    Matrix tempHxH = MatrixView(H, H, v->tempM.entries);         // Temporary matrix [H x H]
 
     // Let y = output
     softmax(output, 1.0f);
@@ -312,45 +302,44 @@ float ModelBackward(Model m, Token input, Token target, Matrix output)
 
     // Output layer
     // dL/dW_o += dL/dy * h^T
-    MatrixMultiply(tempVxH, output, MatrixTranspose(m->hs.h[N - 1]));
-    MatrixAdd(m->g.o.W, m->g.o.W, tempVxH);
+    MatrixMultiply(tempVxH, output, MatrixTranspose(m->v.h[N - 1]));
+    MatrixAdd(m->v.dp.o.W, m->v.dp.o.W, tempVxH);
 
     // dL/db_o += dL/dy
-    MatrixAdd(m->g.o.b, m->g.o.b, output);
+    MatrixAdd(m->v.dp.o.b, m->v.dp.o.b, output);
 
     // dL/dh = W_o^T * dL/dy
     //       = ((dL/dy)^T * W_o)^T 
-    MatrixMultiply(MatrixTranspose(up), MatrixTranspose(output), m->p.o.W);
+    MatrixMultiply(MatrixTranspose(dh), MatrixTranspose(output), m->p.o.W);
 
     // Iterate over MGU layers backwards
     for (size_t l = N; l-- > 0; )
     {
-        Matrix tempH2 = v->h_hat[l]; // Reuse h_hat as temporary variable
-        Matrix tempH3 = v->z_h[l];   // Reuse z_h as temporary variable
-        HiddenLayer p = m->p.h[l];   // Hidden layer parameters
-        HiddenLayer g = m->g.h[l];   // Hidden layer gradients
+        HiddenLayer p = m->p.h[l];     // Hidden layer parameters
+        HiddenLayer dp = m->v.dp.h[l]; // Hidden layer gradients
 
         // dL/dz_h = (dL/dh ⊙ f) ⊙ rationalTanh'(z_h)
-        MatrixApply(v->z_h[l], drationalTanh);
-        MatrixHadamard(tempH1, up, v->f[l]);
+        MatrixCopy(tempH3, v->z_h[l]);
+        MatrixApply(tempH3, drationalTanh);
+        MatrixHadamard(tempH1, dh, v->f[l]);
         MatrixHadamard(tempH3, tempH1, tempH3);
 
         // dL/dW_h += dL/dz_h * x^T
         MatrixMultiply(tempHxH, tempH3, MatrixTranspose(v->x[l]));
-        MatrixAdd(g.W_h, g.W_h, tempHxH);
+        MatrixAdd(dp.W_h, dp.W_h, tempHxH);
 
         // dL/dU_h += dL/dz_h * (f ⊙ h_prev)^T
         MatrixHadamard(tempH1, v->f[l], v->h_prev[l]);
         MatrixMultiply(tempHxH, tempH3, MatrixTranspose(tempH1));
-        MatrixAdd(g.U_h, g.U_h, tempHxH);
+        MatrixAdd(dp.U_h, dp.U_h, tempHxH);
 
         // dL/db_h += dL/dz_h
-        MatrixAdd(g.b_h, g.b_h, tempH3);
+        MatrixAdd(dp.b_h, dp.b_h, tempH3);
 
         // dL/df = dL/dh ⊙ (h_hat - h_prev) + (U_h^T * dL/dz_h) ⊙ h_prev
         //         dL/dh ⊙ (h_hat - h_prev) + ((dL/dz_h)^T * U_h)^T ⊙ h_prev
         MatrixSubtract(tempH1, v->h_hat[l], v->h_prev[l]);
-        MatrixHadamard(tempH2, up, tempH1);
+        MatrixHadamard(tempH2, dh, tempH1);
         MatrixMultiply(MatrixTranspose(tempH1), MatrixTranspose(tempH3), p.U_h);
         MatrixHadamard(tempH1, tempH1, v->h_prev[l]);
         MatrixAdd(tempH2, tempH2, tempH1);
@@ -362,26 +351,28 @@ float ModelBackward(Model m, Token input, Token target, Matrix output)
 
         // dL/dW_f += dL/dz_f * x^T
         MatrixMultiply(tempHxH, tempH2, MatrixTranspose(v->x[l]));
-        MatrixAdd(g.W_f, g.W_f, tempHxH);
+        MatrixAdd(dp.W_f, dp.W_f, tempHxH);
 
         // dL/dU_f += dL/dz_f * h_prev^T
         MatrixMultiply(tempHxH, tempH2, MatrixTranspose(v->h_prev[l]));
-        MatrixAdd(g.U_f, g.U_f, tempHxH);
+        MatrixAdd(dp.U_f, dp.U_f, tempHxH);
 
         // dL/db_f += dL/dz_f
-        MatrixAdd(g.b_f, g.b_f, tempH2);
+        MatrixAdd(dp.b_f, dp.b_f, tempH2);
 
         // dL/dx = W_h^T * dL/dz_h + W_f^T * dL/dz_f
         //       = ((dL/dz_h)^T * W_h)^T + ((dL/dz_f)^T * W_f)^T
-        MatrixMultiply(MatrixTranspose(up), MatrixTranspose(tempH3), p.W_h);
+        MatrixMultiply(MatrixTranspose(dh), MatrixTranspose(tempH3), p.W_h);
         MatrixMultiply(MatrixTranspose(tempH1), MatrixTranspose(tempH2), p.W_f);
-        MatrixAdd(up, up, tempH1);
+        MatrixAdd(dh, dh, tempH1);
+
+        // dL/dx becomes dL/dh for the previous layer
     }
 
     // Embedding layer
     // dL/dW_e[input, :] += dL/dx
-    Matrix dW_e = MatrixTranspose(MatrixRow(m->g.e.W, (size_t)input));
-    MatrixAdd(dW_e, dW_e, up);
+    Matrix dW_e = MatrixTranspose(MatrixRow(m->v.dp.e.W, (size_t)input));
+    MatrixAdd(dW_e, dW_e, dh);
 
     return loss;
 }
